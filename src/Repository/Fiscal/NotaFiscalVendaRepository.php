211fd3569ac82d57b4c6e807b5c9b7884ce69d0c
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2020-05-09 12:35:01 -0300

    ...

--- src/Repository/Fiscal/NotaFiscalVendaRepository.php
+++ src/Repository/Fiscal/NotaFiscalVendaRepository.php
@@ -2,12 +2,8 @@
 
 namespace CrosierSource\CrosierLibRadxBundle\Repository\Fiscal;
 
-use CrosierSource\CrosierLibRadxBundle\Entity\Fiscal\NotaFiscal;
+use CrosierSource\CrosierLibBaseBundle\Repository\FilterRepository;
 use CrosierSource\CrosierLibRadxBundle\Entity\Fiscal\NotaFiscalVenda;
-use CrosierSource\CrosierLibRadxBundle\Entity\Vendas\Venda;
-use CrosierSource\CrosierLibRadxBundle\Utils\Fiscal\NFeUtils;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * Repository para a entidade NotaFiscalVenda.
@@ -15,45 +11,15 @@ use Doctrine\Common\Persistence\ManagerRegistry;
  * @author Carlos Eduardo Pauluk
  *
  */
-class NotaFiscalVendaRepository extends ServiceEntityRepository
+class NotaFiscalVendaRepository extends FilterRepository
 {
 
-
     /**
-     * @var NFeUtils
+     * @return string
      */
-    private $nfeUtils;
-
-    public function __construct(ManagerRegistry $registry, NFeUtils $nfeUtils)
+    public function getEntityClass(): string
     {
-        parent::__construct($registry, NotaFiscalVenda::class);
-        $this->nfeUtils = $nfeUtils;
+        return NotaFiscalVenda::class;
     }
 
-    /**
-     * @param Venda $venda
-     * @return null|NotaFiscalVenda
-     * @throws \CrosierSource\CrosierLibBaseBundle\Exception\ViewException
-     */
-    public function findNotaFiscalByVenda(Venda $venda): ?NotaFiscal
-    {
-        $nfeConfigs = $this->nfeUtils->getNFeConfigsEmUso();
-
-        $ambiente = $nfeConfigs['tpAmb'] === 1 ? 'PROD' : 'HOM';
-
-        $ql = "SELECT nf FROM CrosierSource\CrosierLibRadxBundle\Entity\Fiscal\NotaFiscalVenda nfv JOIN CrosierSource\CrosierLibRadxBundle\Entity\Fiscal\NotaFiscal nf WHERE nfv.notaFiscal = nf AND nfv.venda = :venda AND nf.ambiente = :ambiente";
-        $query = $this->getEntityManager()->createQuery($ql);
-        $query->setParameters([
-            'venda' => $venda,
-            'ambiente' => $ambiente
-        ]);
-
-        $results = $query->getResult();
-
-        if (count($results) > 1) {
-            throw new \LogicException('Mais de uma Nota Fiscal encontrada para [' . $venda->getId() . ']');
-        }
-
-        return count($results) === 1 ? $results[0] : null;
-    }
 }
