d7633bf3916633a29be49a7c1c6283284d7e17ef
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2020-06-19 20:07:49 -0300

    ...

--- src/Repository/Estoque/DeptoRepository.php
+++ src/Repository/Estoque/DeptoRepository.php
@@ -3,8 +3,8 @@
 namespace CrosierSource\CrosierLibRadxBundle\Repository\Estoque;
 
 
-use CrosierSource\CrosierLibRadxBundle\Entity\Estoque\Depto;
 use CrosierSource\CrosierLibBaseBundle\Repository\FilterRepository;
+use CrosierSource\CrosierLibRadxBundle\Entity\Estoque\Depto;
 
 /**
  *
@@ -17,6 +17,82 @@ class DeptoRepository extends FilterRepository
     public function getEntityClass(): string
     {
         return Depto::class;
+    }
+
+    /**
+     * @return false|string
+     */
+    public function buildDeptosGruposSubgruposSelect2(?int $deptoIdSelected = null, ?int $grupoIdSelected = null, ?int $subgrupoIdSelected = null)
+    {
+        $subgrupos = $this->getEntityManager()->getConnection()->fetchAll('SELECT * FROM est_subgrupo ORDER BY json_data->>"$.depto_codigo", json_data->>"$.grupo_codigo", codigo');
+
+        $deptos = [];
+        foreach ($subgrupos as $subgrupo) {
+            if (!$subgrupo['json_data']) continue;
+            $jsonData = json_decode($subgrupo['json_data'], true);
+            if (!isset($deptos[$jsonData['depto_id']])) {
+                $deptos[$jsonData['depto_id']] = [
+                    'id' => $jsonData['depto_id'],
+                    'codigo' => $jsonData['depto_codigo'],
+                    'nome' => $jsonData['depto_nome']
+                ];
+            }
+            if (!isset($deptos[$jsonData['depto_id']]['grupos'][$jsonData['grupo_id']])) {
+                $deptos[$jsonData['depto_id']]['grupos'][$jsonData['grupo_id']] = [
+                    'id' => $jsonData['grupo_id'],
+                    'codigo' => $jsonData['grupo_codigo'],
+                    'nome' => $jsonData['grupo_nome']
+                ];
+            }
+            $deptos[$jsonData['depto_id']]['grupos'][$jsonData['grupo_id']]['subgrupos'][$subgrupo['id']] = [
+                'id' => $subgrupo['id'],
+                'codigo' => $subgrupo['codigo'],
+                'nome' => $subgrupo['nome']
+            ];
+        }
+
+        $sDeptos = [];
+
+        $sDeptos[0] = [
+            'id' => '',
+            'text' => 'Selecione...'
+        ];
+        $d = 1;
+        foreach ($deptos as $depto) {
+            $sDeptos[$d] = [
+                'id' => $depto['id'],
+                'text' => $depto['codigo'] . ' - ' . $depto['nome'],
+                'selected' => (int)$depto['id'] === $deptoIdSelected
+            ];
+            $sDeptos[$d]['grupos'][0] = [
+                'id' => '',
+                'text' => 'Selecione...'
+            ];
+            $g = 1;
+            foreach ($depto['grupos'] as $grupo) {
+                $sDeptos[$d]['grupos'][$g] = [
+                    'id' => $grupo['id'],
+                    'text' => $grupo['codigo'] . ' - ' . $grupo['nome'],
+                    'selected' => (int)$grupo['id'] === $grupoIdSelected
+                ];
+                $sDeptos[$d]['grupos'][$g]['subgrupos'][] = [
+                    'id' => '',
+                    'text' => 'Selecione...'
+                ];
+                foreach ($grupo['subgrupos'] as $subgrupo) {
+                    $sDeptos[$d]['grupos'][$g]['subgrupos'][] = [
+                        'id' => $subgrupo['id'],
+                        'text' => $subgrupo['codigo'] . ' - ' . $subgrupo['nome'],
+                        'selected' => (int)$subgrupo['id'] === $subgrupoIdSelected
+                    ];
+                }
+                $g++;
+            }
+            $d++;
+        }
+
+        return json_encode($sDeptos);
+
     }
 
 }
