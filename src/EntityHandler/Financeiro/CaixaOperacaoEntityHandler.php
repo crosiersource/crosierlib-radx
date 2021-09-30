220fbc28d653620c8db835b2aac64c9991926f46
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2021-09-30 11:01:28 -0300

    INP-115 Tela para abertura e fechamento de caixa

--- src/EntityHandler/Financeiro/CaixaOperacaoEntityHandler.php
+++ src/EntityHandler/Financeiro/CaixaOperacaoEntityHandler.php
@@ -3,8 +3,10 @@
 namespace CrosierSource\CrosierLibRadxBundle\EntityHandler\Financeiro;
 
 use CrosierSource\CrosierLibBaseBundle\EntityHandler\EntityHandler;
+use CrosierSource\CrosierLibBaseBundle\Exception\ViewException;
 use CrosierSource\CrosierLibBaseBundle\Utils\StringUtils\StringUtils;
 use CrosierSource\CrosierLibRadxBundle\Entity\Financeiro\CaixaOperacao;
+use Symfony\Component\Security\Core\Security;
 
 /**
  * @package CrosierSource\CrosierLibRadxBundle\EntityHandler\Financeiro
@@ -13,6 +15,9 @@ use CrosierSource\CrosierLibRadxBundle\Entity\Financeiro\CaixaOperacao;
 class CaixaOperacaoEntityHandler extends EntityHandler
 {
 
+    protected bool $isTransacionalSave = true;
+
+    
     public function getEntityClass()
     {
         return CaixaOperacao::class;
@@ -27,6 +32,18 @@ class CaixaOperacaoEntityHandler extends EntityHandler
         if (!$caixaOperacao->UUID) {
             $caixaOperacao->UUID = StringUtils::guidv4();
         }
+        if ($caixaOperacao->carteira->caixaStatus === 'ABERTO' && $caixaOperacao === 'ABERTURA') {
+            throw new ViewException("Caixa já está aberto.");
+        }
+        if ($caixaOperacao->carteira->caixaStatus === 'FECHADO' && $caixaOperacao === 'FECHAMENTO') {
+            throw new ViewException("Caixa já está fechado.");
+        }
+        if (!in_array("ROLE_FINAN_CAIXAOPERACAO", $caixaOperacao->responsavel->getRoles(), true)) {
+            throw new ViewException("Usuário sem permissão para abrir/fechar caixa.");
+        }
+        
+        $caixaOperacao->carteira->caixaStatus = $caixaOperacao->operacao === 'ABERTURA' ? 'ABERTO' : 'FECHADO';
+        $caixaOperacao->carteira->caixaResponsavel = $caixaOperacao->responsavel;
     }
 
 
