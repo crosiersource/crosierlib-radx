b68ff79bdede97654254148bde03681c73f1f122
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2023-06-24 12:23:12 -0300

    Bug no filtro por carteiras (quando vem já como array)

--- src/ApiPlatform/Financeiro/MovimentacaoRestrainsExtension.php
+++ src/ApiPlatform/Financeiro/MovimentacaoRestrainsExtension.php
@@ -5,32 +5,31 @@ namespace CrosierSource\CrosierLibRadxBundle\ApiPlatform\Financeiro;
 
 
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\ContextAwareQueryCollectionExtensionInterface;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use App\Entity\TotalDoctor\Associado;
 use App\Entity\TotalDoctor\UsuarioClientePj;
 use App\Repository\TotalDoctor\UsuarioClientePjRepository;
-use CrosierSource\CrosierLibBaseBundle\Entity\Security\User;
 use CrosierSource\CrosierLibRadxBundle\Entity\Financeiro\Movimentacao;
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\QueryBuilder;
-use Symfony\Component\Security\Core\Security;
 
 /**
  * @author Carlos Eduardo Pauluk
  */
 class MovimentacaoRestrainsExtension implements ContextAwareQueryCollectionExtensionInterface
 {
-    
-    public function applyToCollection(QueryBuilder $queryBuilder, 
-                                      QueryNameGeneratorInterface $queryNameGenerator, 
-                                      string $resourceClass, 
-                                      string $operationName = null, 
-                                      array $context = [])
+
+    public function applyToCollection(QueryBuilder                $queryBuilder,
+                                      QueryNameGeneratorInterface $queryNameGenerator,
+                                      string                      $resourceClass,
+                                      string                      $operationName = null,
+                                      array                       $context = [])
     {
         if ($resourceClass === Movimentacao::class && ($context['filters']['carteirasIds'] ?? false)) {
-            $carteirasIds = explode(',', $context['filters']['carteirasIds']);
+            if (is_string($context['filters']['carteirasIds'])) {
+                $carteirasIds = explode(',', $context['filters']['carteirasIds']);
+            } elseif (is_array($context['filters']['carteirasIds'])) {
+                $carteirasIds = $context['filters']['carteirasIds'];
+            }
             $rootAlias = $queryBuilder->getRootAliases()[0];
             $queryBuilder
                 ->andWhere($rootAlias . '.carteira IN (:ids)')
