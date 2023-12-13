a055e775f540d560db178bb19aa44ad8dc8c5b83
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2023-12-13 11:43:48 -0300

    Adicionando verificação para caixaOperacao

--- src/Repository/Financeiro/CaixaOperacaoRepository.php
+++ src/Repository/Financeiro/CaixaOperacaoRepository.php
@@ -3,6 +3,7 @@
 namespace CrosierSource\CrosierLibRadxBundle\Repository\Financeiro;
 
 use CrosierSource\CrosierLibBaseBundle\Repository\FilterRepository;
+use CrosierSource\CrosierLibBaseBundle\Utils\DateTimeUtils\DateTimeUtils;
 use CrosierSource\CrosierLibRadxBundle\Entity\Financeiro\CaixaOperacao;
 
 /**
@@ -16,6 +17,19 @@ class CaixaOperacaoRepository extends FilterRepository
     public function getEntityClass(): string
     {
         return CaixaOperacao::class;
+    }
+
+
+    public function getDtCaixaAberto(Carteira $carteira): ?\DateTime
+    {
+        $sql = 'SELECT operacao, dt_operacao FROM fin_caixa_operacao WHERE carteira_id = :carteira ORDER BY dt_operacao DESC LIMIT 1';
+        $rs = $this->getEntityManager()->getConnection()->fetchAllAssociative($sql, ['carteira' => $carteira->getId()]);
+        if ($rs) {
+            if ($rs[0]['operacao'] === 'ABERTURA') {
+                return DateTimeUtils::parseDateStr($rs[0]['dt_operacao']);
+            }
+        }
+        return null;
     }
 
 }
