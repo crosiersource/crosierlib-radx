02b0904ea7a897bb3b2d8d1e7d17be6913efd094
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2025-01-22 18:02:09 -0300

    Adicionando outros campos no Venda

--- src/Entity/Vendas/Venda.php
+++ src/Entity/Vendas/Venda.php
@@ -56,72 +56,93 @@ class Venda implements EntityId
     use EntityIdTrait;
 
     /**
-     *
      * @ORM\Column(name="dt_venda", type="datetime", nullable=false)
      * @Groups("venda")
-     *
      * @var null|DateTime
      */
     public ?DateTime $dtVenda = null;
 
     /**
-     *
      * @ORM\ManyToOne(targetEntity="CrosierSource\CrosierLibRadxBundle\Entity\CRM\Cliente")
      * @ORM\JoinColumn(name="cliente_id")
      * @Groups("venda")
-     *
      * @var null|Cliente
      */
     public ?Cliente $cliente = null;
 
     /**
-     *
      * @ORM\ManyToOne(targetEntity="CrosierSource\CrosierLibRadxBundle\Entity\RH\Colaborador")
      * @ORM\JoinColumn(name="vendedor_id")
      * @Groups("venda")
-     *
      * @var null|Colaborador
      */
     public ?Colaborador $vendedor = null;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="CrosierSource\CrosierLibRadxBundle\Entity\Vendas\PlanoPagto")
+     * @ORM\JoinColumn(name="plano_pagto_id")
+     * @Groups("venda")
+     * @var null|PlanoPagto
+     */
+    public ?PlanoPagto $planoPagto = null;
 
     /**
-     *
      * @ORM\Column(name="subtotal", type="decimal")
      * @Groups("venda")
-     *
      * @var null|float
      */
     public ?float $subtotal = null;
 
     /**
-     *
      * @ORM\Column(name="desconto", type="decimal")
      * @Groups("venda")
-     *
      * @var null|float
      */
     public ?float $desconto = null;
+    
+    /**
+     * @ORM\Column(name="desconto_especial", type="decimal")
+     * @Groups("venda")
+     * @var null|float
+     */
+    public ?float $descontoEspecial = null;
 
     /**
-     *
+     * @ORM\Column(name="historico_desconto", type="string")
+     * @Groups("venda")
+     * @var null|string
+     */
+    public ?string $historicoDesconto = null;
+
+    /**
      * @ORM\Column(name="valor_total", type="decimal")
      * @Groups("venda")
-     *
      * @var null|float
      */
     public ?float $valorTotal = null;
 
     /**
-     *
      * @ORM\Column(name="status", type="string")
      * @Groups("venda")
-     *
      * @var null|string
      */
     public ?string $status = null;
+    
+    /**
+     * @ORM\Column(name="deletado", type="boolean")
+     * @Groups("venda")
+     * @var null|bool
+     */
+    public ?bool $deletado = false;
 
     /**
-     *
+     * @ORM\Column(name="obs", type="string")
+     * @Groups("venda")
+     * @var null|string
+     */
+    public ?string $obs = null;
+
+    /**
      * @ORM\Column(name="json_data", type="json")
      * @var null|array
      * @NotUppercase()
